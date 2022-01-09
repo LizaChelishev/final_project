3ed0d61582a777164ba97e8edad21921c34b47ec
LizaChelishev <90146659+LizaChelishev@users.noreply.github.com>
2022-01-09 20:00:34 +0200

    done

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
-from db_config import create_all_entities
-from db_repo import DbRepo
-from Airline_Companies import Airline_Companies
-from Flights import Flights
-from Tickets import Tickets
-from Countries import Countries
-from Customers import Customers
-from Users import Users
-from User_Roles import User_Roles
-from Administrators import Administrators
+from Flights_Management import Flights_Management
 
-local_session = create_all_entities()
-repo = DbRepo(local_session)
+flights_management = Flights_Management()
 
-
-repo.reset_auto_inc(Airline_Companies)
-repo.reset_auto_inc(Flights)
-repo.reset_auto_inc(Tickets)
-repo.reset_auto_inc(Countries)
-repo.reset_auto_inc(Customers)
-repo.reset_auto_inc(Users)
-repo.reset_auto_inc(User_Roles)
-repo.reset_auto_inc(Administrators)
-
-user_roles_list = [User_Roles(role_name='Customer'), User_Roles(role_name='Airline_Company'),
-                   User_Roles(role_name='Administrator')]
-repo.add_all(user_roles_list)
 
 
 
