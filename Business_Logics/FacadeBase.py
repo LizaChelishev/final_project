17a8f5dd438f93beb73fba0e01040098dc104b8e
LizaChelishev <90146659+LizaChelishev@users.noreply.github.com>
2022-02-14 15:41:43 +0200

    done

--- Business_Logics/FacadeBase.py
+++ Business_Logics/FacadeBase.py
@@ -11,6 +11,7 @@ from Database.Administrators import Administrators
 import logging
 from ApplicationLogger import print_to_log
 from abc import ABC, abstractmethod
+from LoginToken import LoginToken
 
 logger = logging.getLogger(__name__)
 
@@ -20,8 +21,8 @@ class FacadeBase(ABC):
 
     @abstractmethod
     def init(self):
-        print('init')
-        if FacadeBase.repo is None:
+#        print('init')
+#       if FacadeBase.repo is None:
             print('creating DB repo')
             local_session = create_all_entities()
             FacadeBase.repo = DbRepo(local_session)
@@ -62,24 +63,106 @@ class FacadeBase(ABC):
                                 remaining_tickets=10)]
         FacadeBase.repo.add_all(flights_list)
 
+    @property
+    def login_token(self):
+        return self._login_token
+
     def get_all_flights(self):
         print_to_log(logger, logging.INFO, 'Getting all flights...')
+        return self.repo.get_all(Flights)
 
     def get_flight_by_id(self, flight_id):
-        print_to_log(logger, logging.INFO, f'Getting flight id {flight_id}...')
-        flight_dbo = FacadeBase.repo.get_by_id(Flights, flight_id)
-        flight_dto = flight_dbo.get_dto()
-        return flight_dto
+        if not isinstance(id, int):
+            self.logger.logger.error(
+                f'The login token "{self.login_token}" used the function get_flights_by_id '
+                f'but the id "{id}" that was sent is not an integer.')
+            return
+        if id <= 0:
+            self.logger.logger.error(
+                f'The login token "{self.login_token}" used the function get_flights_by_id but the id "{id}" that was '
+                f'sent is not positive.')
+            return self.repo.get_by_condition(Flights, lambda query: query.filter(Flights.id == id).all())
+
+        else:
+            print_to_log(logger, logging.INFO, f'Getting flight id {flight_id}...')
+            flight_dbo = FacadeBase.repo.get_by_id(Flights, flight_id)
+            flight_dto = flight_dbo.get_dto()
+            return flight_dto
 
     def get_flights_by_parameters(self, origin_country_id, destination_country_id, date):
         print_to_log(logger, logging.INFO, f'Getting flight with origin country id:{origin_country_id},'
                                            f' destinations country id:{destination_country_id}, date: {date}.')
+        if not isinstance(origin_country_id, int) or not isinstance(destination_country_id, int):
+            self.logger.logger.error(
+                f'The login token "{self.login_token}" used the function get_flights_by_parameters but the county ids '
+                f'"{origin_country_id}" and "{destination_country_id}" '
+                f'that was sent must be integers')
+            return
+        if origin_country_id <= 0 or destination_country_id <= 0:
+            self.logger.logger.error(
+                f'The login token "{self.login_token}" used the function get_flights_by_parameters but the county ids '
+                f'"{origin_country_id}" and "{destination_country_id}" '
+                f'that was sent must be positive')
+            return
+        if not isinstance(date, datetime):
+            self.logger.logger.error(
+                f'The login token "{self.login_token}" used the function get_flights_by_parameters but the the date '
+                f'"{date}" that was sent must be a Datetime object')
+            return
+        return self.repo.get_by_condition(Flights,
+                                          lambda query: query.filter(Flights.origin_country_id == origin_country_id,
+                                                                     Flights.destination_country_id == destination_country_id,
+                                                                     extract('year',
+                                                                             Flights.departure_time) == date.year,
+                                                                     extract('month',
+                                                                             Flights.departure_time) == date.month,
+                                                                     extract('day',
+                                                                             Flights.departure_time) == date.day).all())
 
     def get_all_airlines(self):
         print_to_log(logger, logging.INFO, 'Getting all airlines...')
+        return self.repo.get_all(Airline_Companies)
 
     def get_airline_by_id(self, id):
         print_to_log(logger, logging.INFO, f'Getting flight id {id}...')
+        if not isinstance(id, int):
+            self.logger.logger.error(
+                f'The login token "{self.login_token}" used the function get_airline_by_id but the id "{id}" '
+                f'that was sent is not an integer.')
+            return
+        if id <= 0:
+            self.logger.logger.error(
+                f'The login token "{self.login_token}" used the function get_airline_by_id but the id "{id}" '
+                f'that was sent is not positive.')
+            return
+        return self.repo.get_by_condition(Airline_Companies, lambda query: query.filter(Airline_Companies.id == id_).all())
+
+    def create_user(self, user):
+        if not isinstance(user, Users):
+            self.logger.logger.error(
+                f'The login token "{self.login_token}" used the function create_user but the user "{user}" '
+                f'that was sent must be instance if the class User.')
+            return
+        if self.repo.get_by_condition(Users, lambda query: query.filter(Users.username == user.username).all()):
+            self.logger.logger.error(
+                f'The login token "{self.login_token}" used the function create_user but the user.username '
+                f'"{user.username}" that was sent already exists in the db.')
+            return
+        if self.repo.get_by_condition(Users, lambda query: query.filter(Users.email == user.email).all()):
+            self.logger.logger.error(
+                f'The login token "{self.login_token}" used the function create_user but the user.email "{user.email}" '
+                f'that was sent already exists in the db.')
+            return
+        if not self.repo.get_by_condition(User_Roles, lambda query: query.filter(User_Roles.id == user.user_role).all()):
+            self.logger.logger.error(
+                f'The login token "{self.login_token}" used the function create_user but the user.user_role '
+                f'"{user.user_role}" that was sent does not exist in the db.')
+            return
+        user.id = None
+        self.logger.logger.debug(f'The login token "{self.login_token}" used the function create_user and new user '
+                                 f'"{user}" has ben added to the db.')
+        self.repo.add(user)
+        return True
 
     def get_airline_by_parameter(self, name, country_id, user_id):
         print_to_log(logger, logging.INFO, f'Getting airline company with name {name}...')
@@ -92,6 +175,18 @@ class FacadeBase(ABC):
 
     def get_all_countries(self):
         print_to_log(logger, logging.INFO, 'Getting all countries...')
+        return self.repo.get_all(Countries)
 
     def get_country_by_id(self, id):
         print_to_log(logger, logging.INFO, f'Getting country id {id}...')
+        if not isinstance(id, int):
+            self.logger.logger.error(
+                f'The login token "{self.login_token}" used the function get_country_by_id but the id "{id}" that was '
+                f'sent is not an integer.')
+            return
+        if id_ <= 0:
+            self.logger.logger.error(
+                f'The login token "{self.login_token}" used the function get_country_by_id but the id "{id}" that was '
+                f'sent is not positive.')
+            return
+        return self.repo.get_by_condition(Countries, lambda query: query.filter(Countries.id == id).all())
--- db_repo.py
+++ db_repo.py
@@ -1,10 +1,15 @@
 import datetime
+from sqlalchemy import asc, extract, desc, text
 
-from sqlalchemy import asc
-
+from Database.Administrators import Administrators
+from Database.Airline_Companies import Airline_Companies
+from Database.Countries import Countries
+from Database.Customers import Customers
 from Database.Flights import Flights
 from Database.Tickets import Tickets
+from Database.User_Roles import User_Roles
 from Database.Users import Users
+from sqlalchemy.exc import OperationalError, IntegrityError
 
 
 class DbRepo:
@@ -12,13 +17,287 @@ class DbRepo:
         self.local_session = local_session
 
     def reset_auto_inc(self, table_class):
-        self.local_session.execute(f'TRUNCATE TABLE {table_class.__tablename__} RESTART IDENTITY CASCADE')
+        try:
+            self.local_session.execute(f'TRUNCATE TABLE {table_class.__tablename__} RESTART IDENTITY CASCADE')
+            self.local_session.commit()
+            self.logger.logger.debug(f'Reset auto inc in {table_class} table')
+        except OperationalError as e:
+            self.logger.logger.critical(e)
 
     def get_by_id(self, table_class, id):
         return self.local_session.query(table_class).get(id)
 
     def get_all(self, table_class):
-        return self.local_session.query(table_class).all()
+        try:
+            return self.local_session.query(table_class).all()
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def get_all_limit(self, table_class, limit_num):
+        try:
+            return self.local_session.query(table_class).limit(limit_num).all()
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def get_all_order_by(self, table_class, column_name, direction=asc):
+        try:
+            return self.local_session.query(table_class).order_by(direction(column_name)).all()
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def get_by_column_value(self, table_class, column_value, value):
+        try:
+            return self.local_session.query(table_class).filter(column_value == value).all()
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def get_by_condition(self, table_class, condition):  # condition is a lambda expression of a filter
+        try:
+            return condition(self.local_session.query(table_class))
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def add(self, one_row):
+        try:
+            self.local_session.add(one_row)
+            self.local_session.commit()
+            self.logger.logger.debug(f'{one_row} has been added to the db')
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def add_all(self, rows_list):
+        try:
+            self.local_session.add_all(rows_list)
+            self.local_session.commit()
+            self.logger.logger.debug(f'{rows_list} have been added to the db')
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def delete_by_id(self, table_class, id_column_name, id):
+        try:
+            self.local_session.query(table_class).filter(id_column_name == id).delete(synchronize_session=False)
+            self.local_session.commit()
+            self.logger.logger.debug(f'A row with the id {id} has been deleted from {table_class}')
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def update_by_id(self, table_class, id_column_name, id,
+                     data):  # data is a dictionary of all the new columns and values
+        try:
+            self.local_session.query(table_class).filter(id_column_name == id).update(data)
+            self.local_session.commit()
+            self.logger.logger.debug(
+                f'A row with the id {id} has been updated from {table_class}. the updated data is  {data}.')
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def get_airlines_by_country(self, country_id):
+        try:
+            return self.local_session.query(Airline_Companies).filter(Airline_Companies.country_id == country_id).all()
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def get_flights_by_destination_country_id(self, country_id):
+        try:
+            return self.local_session.query(Flights).filter(Flights.destination_country_id == country_id).all()
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def get_flights_by_origin_country_id(self, country_id):
+        try:
+            return self.local_session.query(Flights).filter(Flights.origin_country_id == country_id).all()
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def get_flights_by_departure_date(self, departure_date):
+        try:
+            return self.local_session.query(Flights).filter(
+                extract('year', Flights.departure_date) == departure_date.year,
+                extract('month', Flights.departure_date) == departure_date.month,
+                extract('day', Flights.departure_date) == departure_date.day).all()
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def get_flights_by_landing_date(self, landing_date):
+        try:
+            return self.local_session.query(Flights).filter(
+                extract('year', Flights.departure_date) == landing_date.year,
+                extract('month',
+                        Flights.departure_date) == landing_date.month,
+                extract('day',
+                        Flights.departure_date) == landing_date.day).all()
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def get_flights_by_customer(self, customer_id):
+        try:
+            flights_ls = []
+            tickets = self.local_session.query(Tickets).filter(Tickets.customer_id == customer_id).all()
+            for ticket in tickets:
+                flights_ls.append(ticket.flight)
+            return flights_ls
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def sp_get_airline_by_username(self, _username):
+        try:
+            stmt = text('select * from sp_get_airline_by_username(:username)').bindparams(username=_username)
+            airline_cursor = self.local_session.execute(stmt)
+            airline = [air1 for air1 in airline_cursor][0]
+            return airline
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def sp_get_customer_by_username(self, _username):
+        try:
+            stmt = text('select * from sp_get_customer_by_username(:username)').bindparams(username=_username)
+            customer_cursor = self.local_session.execute(stmt)
+            customer = [cus1 for cus1 in customer_cursor][0]
+            return customer
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def sp_get_user_by_username(self, _username):
+        try:
+            stmt = text('select * from sp_get_user_by_username(:username)').bindparams(username=_username)
+            user_cursor = self.local_session.execute(stmt)
+            user = [us1 for us1 in user_cursor][0]
+            return user
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def sp_get_flights_by_airline_id(self, _airline_id):
+        try:
+            stmt = text('select * from sp_get_flights_by_airline_id(:airline_id)').bindparams(airline_id=_airline_id)
+            flights_cursor = self.local_session.execute(stmt)
+            flights = [flight for flight in flights_cursor]
+            return flights
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def sp_get_tickets_by_customer_id(self, _customer_id):
+        try:
+            stmt = text('select * from sp_get_tickets_by_customer_id(:customer_id)').bindparams(
+                customer_id=_customer_id)
+            tickets_cursor = self.local_session.execute(stmt)
+            tickets = [ticket for ticket in tickets_cursor]
+            return tickets
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def sp_get_arrival_flights(self,
+                               _country_id):  # returns all the flights that arrive to the country_id in the next 12 hours
+        try:
+            stmt = text('select * from sp_get_arrival_flights(:country_id)').bindparams(country_id=_country_id)
+            flights_cursor = self.local_session.execute(stmt)
+            flights = [flight for flight in flights_cursor]
+            return flights
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def sp_get_departure_flights(self,
+                                 _country_id):  # returns all the flights that departure to the country_id in the next 12 hours
+        try:
+            stmt = text('select * from sp_get_departure_flights(:country_id)').bindparams(country_id=_country_id)
+            flights_cursor = self.local_session.execute(stmt)
+            flights = [flight for flight in flights_cursor]
+            return flights
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def sp_get_flights_by_parameters(self, _origin_country_id, _destination_country_id, _date):
+        try:
+            stmt = text(
+                'select * from sp_get_flights_by_parameters(:origin_country_id, :destination_country_id, :date)') \
+                .bindparams(origin_country_id=_origin_country_id, destination_country_id=_destination_country_id,
+                            date=_date)
+            flights_cursor = self.local_session.execute(stmt)
+            flights = [flight for flight in flights_cursor]
+            return flights
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def create_all_sp(self, file):
+        try:
+            try:
+                with open(file, 'r') as sp_file:
+                    queries = sp_file.read().split('|||')
+                for query in queries:
+                    self.local_session.execute(query)
+                self.local_session.commit()
+                self.logger.logger.debug(f'all sp from {file} were created.')
+            except FileNotFoundError:
+                self.logger.logger.critical(f'Tried to create all sp from the the file "{file}" but file was not found')
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def drop_all_tables(self):
+        try:
+            self.local_session.execute('DROP TABLE users CASCADE')
+            self.local_session.execute('DROP TABLE user_roles CASCADE')
+            self.local_session.execute('DROP TABLE tickets CASCADE')
+            self.local_session.execute('DROP TABLE flights CASCADE')
+            self.local_session.execute('DROP TABLE customers CASCADE')
+            self.local_session.execute('DROP TABLE countries CASCADE')
+            self.local_session.execute('DROP TABLE airline_companies CASCADE')
+            self.local_session.execute('DROP TABLE administrators CASCADE')
+            self.local_session.commit()
+            self.logger.logger.debug(f'All tables Dropped.')
+        except OperationalError as e:
+            self.logger.logger.critical(e)
+
+    def reset_test_db(self):
+        try:
+            # resetting auto increment for all tables
+            self.reset_auto_inc(Countries)
+            self.reset_auto_inc(User_Roles)
+            self.reset_auto_inc(Users)
+            self.reset_auto_inc(Administrators)
+            self.reset_auto_inc(Airline_Companies)
+            self.reset_auto_inc(Customers)
+            self.reset_auto_inc(Flights)
+            self.reset_auto_inc(Tickets)
+            # county
+            israel = Countries(name='Israel')
+            self.add(israel)
+            self.add(Countries(name='Germany'))
+            # user role
+            self.add(User_Roles(role_name='Customer'))
+            self.add(User_Roles(role_name='Airline Company'))
+            self.add(User_Roles(role_name='Administrator'))
+            self.add(User_Roles(role_name='Not Legal'))
+            # user
+            self.add(Users(username='Elad', password='123', email='elad@gmail.com', user_role=1))
+            self.add(Users(username='Uri', password='123', email='uri@gmail.com', user_role=1))
+            self.add(Users(username='Yoni', password='123', email='yoni@gmail.com', user_role=2))
+            self.add(Users(username='Yishay', password='123', email='yishay@gmail.com', user_role=2))
+            self.add(Users(username='Tomer', password='123', email='tomer@gmail.com', user_role=3))
+            self.add(Users(username='Boris', password='123', email='boris@gmail.com', user_role=3))
+            self.add(Users(username='not legal', password='123', email='notlegal@gmail.com', user_role=4))
+            # administrator
+            self.add(Administrators(first_name='Tomer', last_name='Tome', user_id=5))
+            self.add(Administrators(first_name='Boris', last_name='Bori', user_id=6))
+            # airline company
+            self.add(Airline_Companies(name='Yoni', country_id=1, user_id=3))
+            self.add(Airline_Companies(name='Yishay', country_id=2, user_id=4))
+            # customer
+            self.add(Customers(first_name='Elad', last_name='Gunders', address='Sokolov 11',
+                               phone_no='0545557007', credit_card_no='0000', user_id=1))
+            self.add(Customers(first_name='Uri', last_name='Goldshmid', address='Helsinki 16',
+                               phone_no='0527588331', credit_card_no='0001', user_id=2))
+            # flight
+            self.add(Flights(airline_company_id=1, origin_country_id=1, destination_country_id=2,
+                             departure_time=datetime(2022, 1, 30, 16, 0, 0),
+                             landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=200))
+            self.add(Flights(airline_company_id=2, origin_country_id=1, destination_country_id=2,
+                             departure_time=datetime(2022, 1, 30, 16, 0, 0),
+                             landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=0))
+            # ticket
+            self.add(Tickets(flight_id=1, customer_id=1))
+            self.add(Tickets(flight_id=2, customer_id=2))
+            self.logger.logger.debug(f'Reset flights_db_tests')
+        except OperationalError as e:
+            self.logger.logger.critical(e)
 
     def get_all_by_condition(self, table_class, filter_column, filter_value):
         return self.local_session.query(table_class).filter(filter_column == filter_value)
@@ -37,7 +316,7 @@ class DbRepo:
         self.local_session.add(table_class, object)
         self.local_session.commit()
 
-    def update(self, table_class, id_column_name,  id, data_dict):
+    def update(self, table_class, id_column_name, id, data_dict):
         self.local_session.query(table_class).filter(id_column_name == id).update(data_dict)
         self.local_session.commit()
 
@@ -66,7 +345,7 @@ class DbRepo:
 
     def get_arrival_flights(self, country_id):
         return self.local_session.query(Flights).filter(country_id in range(datetime.datetime.now(),
-                                                                              datetime.timedelta(hours=12))).all()
+                                                                            datetime.timedelta(hours=12))).all()
 
     def get_departure_flights(self, country_id):
         return self.local_session.query(Flights).filter(country_id in range(datetime.datetime.now(),
@@ -92,4 +371,3 @@ class DbRepo:
 
     def getFlightsByCustomer(self, customer):
         pass
-
