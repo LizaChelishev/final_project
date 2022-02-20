9485c2ced6aa21e48df8519c1890f71960e2bcda
LizaChelishev <90146659+LizaChelishev@users.noreply.github.com>
2022-02-20 20:36:14 +0200

    done

--- Tests/AirlineFacadeTest.py
+++ Tests/AirlineFacadeTest.py
@@ -26,24 +26,37 @@ def reset_db(airline_facade_object):
 def test_airline_facade_get_airline_flights(airline_facade_object):
     actual = airline_facade_object.get_airline_flights()
     assert actual == [Flights(id=1, airline_company_id=1, origin_country_id=1, destination_country_id=2,
-                             departure_time=datetime(2022, 1, 30, 16, 0, 0), landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=200)]
+                              departure_time=datetime(2022, 1, 30, 16, 0, 0),
+                              landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=200)]
 
 
 @pytest.mark.parametrize('flight, expected', [('not flight', None),
                                               (Flights(origin_country_id=3, destination_country_id=2,
-                        departure_time=datetime(2022, 1, 30, 16, 0, 0), landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=200), None),
+                                                       departure_time=datetime(2022, 1, 30, 16, 0, 0),
+                                                       landing_time=datetime(2022, 1, 30, 20, 0, 0),
+                                                       remaining_tickets=200), None),
                                               (Flights(origin_country_id=1, destination_country_id=3,
-                        departure_time=datetime(2022, 1, 30, 16, 0, 0), landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=200), None),
+                                                       departure_time=datetime(2022, 1, 30, 16, 0, 0),
+                                                       landing_time=datetime(2022, 1, 30, 20, 0, 0),
+                                                       remaining_tickets=200), None),
                                               (Flights(origin_country_id=1, destination_country_id=2,
-                        departure_time=1, landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=200), None),
+                                                       departure_time=1, landing_time=datetime(2022, 1, 30, 20, 0, 0),
+                                                       remaining_tickets=200), None),
                                               (Flights(origin_country_id=1, destination_country_id=2,
-                        departure_time=datetime(2022, 1, 30, 16, 0, 0), landing_time='not datetime', remaining_tickets=200), None),
+                                                       departure_time=datetime(2022, 1, 30, 16, 0, 0),
+                                                       landing_time='not datetime', remaining_tickets=200), None),
                                               (Flights(origin_country_id=1, destination_country_id=2,
-                        departure_time=datetime(2022, 1, 30, 16, 0, 0), landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=100.7), None),
+                                                       departure_time=datetime(2022, 1, 30, 16, 0, 0),
+                                                       landing_time=datetime(2022, 1, 30, 20, 0, 0),
+                                                       remaining_tickets=100.7), None),
                                               (Flights(origin_country_id=1, destination_country_id=2,
-                        departure_time=datetime(2022, 1, 30, 16, 0, 0), landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=99), None),
+                                                       departure_time=datetime(2022, 1, 30, 16, 0, 0),
+                                                       landing_time=datetime(2022, 1, 30, 20, 0, 0),
+                                                       remaining_tickets=99), None),
                                               (Flights(origin_country_id=1, destination_country_id=2,
-                        departure_time=datetime(2022, 1, 30, 17, 0, 0), landing_time=datetime(2022, 1, 30, 21, 0, 0), remaining_tickets=100), True)])
+                                                       departure_time=datetime(2022, 1, 30, 17, 0, 0),
+                                                       landing_time=datetime(2022, 1, 30, 21, 0, 0),
+                                                       remaining_tickets=100), True)])
 def test_airline_facade_add_flight(airline_facade_object, flight, expected):
     actual = airline_facade_object.add_flight(flight)
     assert actual == expected
@@ -52,7 +65,8 @@ def test_airline_facade_add_flight(airline_facade_object, flight, expected):
 def test_airline_facade_add_flight_raise_notlegalflighttimeserror(airline_facade_object):
     with pytest.raises(FlightTimesException):
         airline_facade_object.add_flight(Flights(origin_country_id=1, destination_country_id=2,
-                                                departure_time=datetime(2022, 1, 30, 17, 0, 0), landing_time=datetime(2022, 1, 30, 17, 59, 0), remaining_tickets=100))
+                                                 departure_time=datetime(2022, 1, 30, 17, 0, 0),
+                                                 landing_time=datetime(2022, 1, 30, 17, 59, 0), remaining_tickets=100))
 
 
 @pytest.mark.parametrize('airline, expected', [('not airline', None),
@@ -65,26 +79,54 @@ def test_airline_facade_update_airline(airline_facade_object, airline, expected)
 
 
 @pytest.mark.parametrize('flight, expected', [('not flight', None),
-                                              (Flights(airline_company_id=3, origin_country_id=1, destination_country_id=2,
-                        departure_time=datetime(2022, 1, 30, 16, 0, 0), landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=200), None),
-                                              (Flights(airline_company_id=1, origin_country_id=3, destination_country_id=2,
-                        departure_time=datetime(2022, 1, 30, 16, 0, 0), landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=200), None),
-                                              (Flights(airline_company_id=1, origin_country_id=1, destination_country_id=3,
-                        departure_time=datetime(2022, 1, 30, 16, 0, 0), landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=200), None),
-                                              (Flights(airline_company_id=1, origin_country_id=1, destination_country_id=2,
-                        departure_time=1, landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=200), None),
-                                              (Flights(airline_company_id=1, origin_country_id=1, destination_country_id=2,
-                        departure_time=datetime(2022, 1, 30, 16, 0, 0), landing_time='not datetime', remaining_tickets=200), None),
-                                              (Flights(airline_company_id=1, origin_country_id=1, destination_country_id=2,
-                        departure_time=datetime(2022, 1, 30, 16, 0, 0), landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=100.7), None),
-                                              (Flights(airline_company_id=1, origin_country_id=1, destination_country_id=2,
-                        departure_time=datetime(2022, 1, 30, 16, 0, 0), landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=-2), None),
-                                              (Flights(id=3, airline_company_id=1, origin_country_id=2, destination_country_id=1,
-                        departure_time=datetime(2022, 1, 29, 17, 0, 0), landing_time=datetime(2022, 1, 30, 14, 0, 0), remaining_tickets=0), None),
-                                              (Flights(id=2, airline_company_id=2, origin_country_id=2, destination_country_id=1,
-                        departure_time=datetime(2022, 1, 29, 17, 0, 0), landing_time=datetime(2022, 1, 30, 14, 0, 0), remaining_tickets=0), None),
-                                              (Flights(id=1, airline_company_id=1, origin_country_id=2, destination_country_id=1,
-                        departure_time=datetime(2022, 1, 29, 17, 0, 0), landing_time=datetime(2022, 1, 30, 14, 0, 0), remaining_tickets=0), True)])
+                                              (Flights(airline_company_id=3, origin_country_id=1,
+                                                       destination_country_id=2,
+                                                       departure_time=datetime(2022, 1, 30, 16, 0, 0),
+                                                       landing_time=datetime(2022, 1, 30, 20, 0, 0),
+                                                       remaining_tickets=200), None),
+                                              (Flights(airline_company_id=1, origin_country_id=3,
+                                                       destination_country_id=2,
+                                                       departure_time=datetime(2022, 1, 30, 16, 0, 0),
+                                                       landing_time=datetime(2022, 1, 30, 20, 0, 0),
+                                                       remaining_tickets=200), None),
+                                              (Flights(airline_company_id=1, origin_country_id=1,
+                                                       destination_country_id=3,
+                                                       departure_time=datetime(2022, 1, 30, 16, 0, 0),
+                                                       landing_time=datetime(2022, 1, 30, 20, 0, 0),
+                                                       remaining_tickets=200), None),
+                                              (Flights(airline_company_id=1, origin_country_id=1,
+                                                       destination_country_id=2,
+                                                       departure_time=1, landing_time=datetime(2022, 1, 30, 20, 0, 0),
+                                                       remaining_tickets=200), None),
+                                              (Flights(airline_company_id=1, origin_country_id=1,
+                                                       destination_country_id=2,
+                                                       departure_time=datetime(2022, 1, 30, 16, 0, 0),
+                                                       landing_time='not datetime', remaining_tickets=200), None),
+                                              (Flights(airline_company_id=1, origin_country_id=1,
+                                                       destination_country_id=2,
+                                                       departure_time=datetime(2022, 1, 30, 16, 0, 0),
+                                                       landing_time=datetime(2022, 1, 30, 20, 0, 0),
+                                                       remaining_tickets=100.7), None),
+                                              (Flights(airline_company_id=1, origin_country_id=1,
+                                                       destination_country_id=2,
+                                                       departure_time=datetime(2022, 1, 30, 16, 0, 0),
+                                                       landing_time=datetime(2022, 1, 30, 20, 0, 0),
+                                                       remaining_tickets=-2), None),
+                                              (Flights(id=3, airline_company_id=1, origin_country_id=2,
+                                                       destination_country_id=1,
+                                                       departure_time=datetime(2022, 1, 29, 17, 0, 0),
+                                                       landing_time=datetime(2022, 1, 30, 14, 0, 0),
+                                                       remaining_tickets=0), None),
+                                              (Flights(id=2, airline_company_id=2, origin_country_id=2,
+                                                       destination_country_id=1,
+                                                       departure_time=datetime(2022, 1, 29, 17, 0, 0),
+                                                       landing_time=datetime(2022, 1, 30, 14, 0, 0),
+                                                       remaining_tickets=0), None),
+                                              (Flights(id=1, airline_company_id=1, origin_country_id=2,
+                                                       destination_country_id=1,
+                                                       departure_time=datetime(2022, 1, 29, 17, 0, 0),
+                                                       landing_time=datetime(2022, 1, 30, 14, 0, 0),
+                                                       remaining_tickets=0), True)])
 def test_airline_facade_update_flight(airline_facade_object, flight, expected):
     actual = airline_facade_object.update_flight(flight)
     assert actual == expected
@@ -92,15 +134,18 @@ def test_airline_facade_update_flight(airline_facade_object, flight, expected):
 
 def test_airline_facade_update_flight_raise_notlegalflighttimeserror(airline_facade_object):
     with pytest.raises(FlightTimesException):
-        airline_facade_object.update_flight(Flights(id=1, airline_company_id=1, origin_country_id=1, destination_country_id=2,
-                                                   departure_time=datetime(2022, 1, 30, 17, 0, 0), landing_time=datetime(2022, 1, 30, 17, 59, 0), remaining_tickets=100))
+        airline_facade_object.update_flight(
+            Flights(id=1, airline_company_id=1, origin_country_id=1, destination_country_id=2,
+                    departure_time=datetime(2022, 1, 30, 17, 0, 0), landing_time=datetime(2022, 1, 30, 17, 59, 0),
+                    remaining_tickets=100))
 
 
 def test_airline_facade_update_flight_raise_noremainingticketserror(airline_facade_object):
     with pytest.raises(NoMoreTicketsForFlightsException):
-        airline_facade_object.update_flight(Flights(id=1, airline_company_id=1, origin_country_id=1, destination_country_id=2,
-                                                   departure_time=datetime(2022, 1, 29, 00, 0, 0),
-                                                   landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=-5))
+        airline_facade_object.update_flight(
+            Flights(id=1, airline_company_id=1, origin_country_id=1, destination_country_id=2,
+                    departure_time=datetime(2022, 1, 29, 00, 0, 0),
+                    landing_time=datetime(2022, 1, 30, 20, 0, 0), remaining_tickets=-5))
 
 
 @pytest.mark.parametrize('flight_id, expected', [('not_id', None),
