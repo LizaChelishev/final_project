d2f28f2bf534a4934913ff214991e12117dc01dc
Liza <liza@minutemedia.com>
2022-09-13 23:12:29 +0300

    done

---
+++ flask_rest_api.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, jsonify
+from flask_cors import CORS
+from Business_Logics.AnonymousFacade import AnonymousFacade
+from DataAccess.db_repo_pool import db_repo_pool
+from templates import *
+
+app = Flask(__name__)
+CORS(app)
+
+repool = db_repo_pool.get_instance()
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/flights/tables")
+def flights_tables():
+    return render_template('flights_tables.html')
+
+
+@app.route("/flights", methods=['GET'])
+def get_all_flights():
+    repo = repool.get_connection()  # getting a repo
+    anonfcade = AnonymousFacade(repo)
+    all_flights = anonfcade.get_all_flights()
+    if all_flights:
+        flights = [flight.data_for_web() for flight in all_flights]
+    else:
+        flights = []
+    repool.return_connection(repo)  # returning the repo
+    return jsonify(flights)
+
+
+@app.route("/arrivals/<int:hours_num>", methods=['GET'])
+def get_arrival_flights_by_delta_t(hours_num):
+    repo = repool.get_connection()  # getting a repo
+    anonfcade = AnonymousFacade(repo)
+    arrivals = anonfcade.get_arrival_flights_by_delta_t(hours_num)
+    if arrivals:
+        flights = [flight.data_for_web() for flight in arrivals]
+    else:
+        flights = []
+    repool.return_connection(repo)  # returning the repo
+    return jsonify(flights)
+
+
+@app.route("/departures/<int:hours_num>", methods=['GET'])
+def get_departure_flights_by_delta_t(hours_num):
+    print(type(hours_num))
+    repo = repool.get_connection()  # getting a repo
+    anonfcade = AnonymousFacade(repo)
+    departures = anonfcade.get_departure_flights_by_delta_t(hours_num)
+    if departures:
+        flights = [flight.data_for_web() for flight in departures]
+    else:
+        flights = []
+    repool.return_connection(repo)  # returning the repo
+    return jsonify(flights)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from DataAccess.db_repo import *
 
 repo = DbRepo(local_session)
 create_all_entities()  # create tables if not exist
-repo.create_all_sp('StoredProceduresFlights.sql')
+repo.create_all_sp('sp_flights_db.sql')
+repo.reset_all_tables_auto_inc()
